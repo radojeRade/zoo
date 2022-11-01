73c50bae8f76f69936ecce8d1ed0d031c4daaf57
radojeRade <aleksandravidic88@gmail.com>
2022-11-01 19:10:11 +0100

    Zadatak 2

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -8,6 +8,7 @@ function AnimalList() {
         {id:3, name: 'Slavka', species: 'Kokoska', dateOfBirth: new Date() },
         {id:4, name: 'Tresnja', species: 'Macka', dateOfBirth: new Date() },
         {id:5, name: 'Jovanka', species: 'Zirafa', dateOfBirth: new Date() },
+        {id:6, name: 'Julka', species: 'Ovca'},
       ]);
 
 
@@ -20,12 +21,13 @@ function AnimalList() {
                     <th>Birth</th>
                 </tr>       
                     {animal.map((item) => (
+                      
                     <tr key={item.id}>
                         <td >{item.species}</td>
                         <td> {item.name}</td>
-                        <td>{item.dateOfBirth.toString()}</td>
+                        <td>{item.dateOfBirth ? item.dateOfBirth.toString() : "Nepoznat"}</td>
 
-                 </tr>
+                    </tr>
                   ))}
             </table>
         </div>
