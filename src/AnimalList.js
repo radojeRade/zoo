b4364142bde7ccca10f9f606bcefe2058d500697
radojeRade <aleksandravidic88@gmail.com>
2022-11-01 20:17:33 +0100

    Zadatak 3

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function AnimalList() {
-
+    const [id, setId] = useState();
     const [animal, setAnimal] = useState([
         {id:1, name: 'Jagoda', species: 'Svinja', dateOfBirth: new Date() },
         {id:2, name: 'Visnja', species: 'Krava', dateOfBirth: new Date() },
@@ -10,25 +10,39 @@ function AnimalList() {
         {id:5, name: 'Jovanka', species: 'Zirafa', dateOfBirth: new Date() },
         {id:6, name: 'Julka', species: 'Ovca'},
       ]);
+      const handleClick = (id) => {
+        //console.log(id);
+         let newAnimals = animal.map(x => x.id).indexOf(id);
+         let arr = [...animal];
+         arr.splice(newAnimals, 1);
+         
+         setAnimal(arr);
+        
+      };
+    
 
 
     return (
         <div>
             <table>
+                <thead>
                 <tr>
                     <th>Species</th>
                     <th>Name</th>
                     <th>Birth</th>
-                </tr>       
+                </tr> 
+                </thead>
+                <tbody>     
                     {animal.map((item) => (
                       
                     <tr key={item.id}>
                         <td >{item.species}</td>
                         <td> {item.name}</td>
                         <td>{item.dateOfBirth ? item.dateOfBirth.toString() : "Nepoznat"}</td>
-
+                        <td><button onClick={()=> handleClick(item.id)}>Remove</button></td>
                     </tr>
                   ))}
+                  </tbody> 
             </table>
         </div>
     )
