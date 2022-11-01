33727a1938aceedf625561227aa8a85bbf8ee9ed
radojeRade <aleksandravidic88@gmail.com>
2022-11-01 19:00:23 +0100

    Zadatak 1

---
+++ src/AnimalList.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function AnimalList() {
+
+    const [animal, setAnimal] = useState([
+        {id:1, name: 'Jagoda', species: 'Svinja', dateOfBirth: new Date() },
+        {id:2, name: 'Visnja', species: 'Krava', dateOfBirth: new Date() },
+        {id:3, name: 'Slavka', species: 'Kokoska', dateOfBirth: new Date() },
+        {id:4, name: 'Tresnja', species: 'Macka', dateOfBirth: new Date() },
+        {id:5, name: 'Jovanka', species: 'Zirafa', dateOfBirth: new Date() },
+      ]);
+
+
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>Species</th>
+                    <th>Name</th>
+                    <th>Birth</th>
+                </tr>       
+                    {animal.map((item) => (
+                    <tr key={item.id}>
+                        <td >{item.species}</td>
+                        <td> {item.name}</td>
+                        <td>{item.dateOfBirth.toString()}</td>
+
+                 </tr>
+                  ))}
+            </table>
+        </div>
+    )
+  }
+  export default AnimalList;
